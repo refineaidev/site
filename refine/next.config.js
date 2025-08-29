1383de22be3ca1bb1471cc86d47fb96234baa244
Emad Alghamdi <37039039+Eaalghamdi@users.noreply.github.com>
2025-08-29 23:27:11 +0300

    added cover images

--- refine/next.config.js
+++ refine/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "img.freepik.com",
+        hostname: "i.postimg.cc",
       },
       // Allow localhost only in development
       ...(process.env.NODE_ENV === "development"
